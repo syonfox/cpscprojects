16ab3948fbb83bc139548807fbd677fb8d626c89
Kier <kier.lightning@gmail.com>
2016-01-15 13:19:37 -0800

    Update Person.java

--- cpsc101/assignment_1/Person.java
+++ cpsc101/assignment_1/Person.java
@@ -1,132 +1,117 @@
-/**
+ /**
 * cpsc101 lab 1
 * date: January 2016
 * Made By:
 *  Kier Lindsay
-**/
+*  Raymond Strohschein
+ **/
 
-
-//import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Arrays;
-import java.util.Random;
+ 
+public class Person {
 
-import java.io.RandomAccessFile;
-import java.io.File;
-import java.io.IOException;
-import java.io.FileNotFoundException;
+/**Variables**/
 
-public class PersonTester{
+  private static int population = 0;
+  private static int alive = 0;
+  private static Person lastPerson = null;
 
+ 
+  private Person murderer;
+  private Person previousPerson;
+  private String name; 
+  private boolean isAlive;
+ 
 
-  //Returns a random line(name since there is a name on each line) form a file and returns it as a string
-  //this method is modified form a stackoverflow post.
-  //link to original post http://stackoverflow.com/a/19850005
-  
-  //NOTE only 4080 names can be generated after that the 
-  //name fred will be used and filenotfound error will be printed
-  public static String getRandomName() {
-    String name = "Fred";
-    try{
-      RandomAccessFile f = new RandomAccessFile("namelist", "r");
-      long randomLocation = (long) (Math.random() * f.length());
-      f.seek(randomLocation);
-      f.readLine();
-      name = f.readLine();
-    
-    } catch(FileNotFoundException e) {
-      System.out.println("Error: namelist File Not found");
-    } catch(IOException e) {
-      System.out.println("Error: IO Exeption");
-    }
+  /**Constructor**/
+
    
-    return name;
+  public Person(String n) {
+    name = n;
+    murderer = null;
+    isAlive = true;
+    population++;
+    alive++;
+    
+    
+    //makes a linkes list of persons so that every person points to the previoous person
+    //null <- p1 <- p2 <- p3 <-p4 ...
+    previousPerson = lastPerson;
+    lastPerson = this;    
+  }
+ 
+ 
+/**Attributes**/
+ 
+  //returns this persons murderer, Murderer is set when somone is murdered( see the murder method ).
+  public Person murderer() {
+    return this.murderer;
+  }
+  
+  //return a persons name, if the person is dead it adds deceased after their name.
+  public String name() {
+   String returnString = name;
+    if(!isAlive) returnString += ", deceased" ;
+    return returnString;
+  }
+  
+  //boolean method to cheack wether a person is alive or not
+  public boolean isAlive() {
+    if(isAlive) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+   
+ /**Actions**/
+ 
+  //causes a person to die and changes the number of alive persons
+  public void die() {
+    if(isAlive) {
+      isAlive=false;
+      alive--;
+    }
+ }
+  
+  //is uses to make a person murder another person,  makes the murderer of a person knowen to that person.
+  public void murder(Person victim) {
+    victim.die();
+    victim.murderer = this;
+  }
+  
+  //causes a person to say hello,  dead persons canot say hello
+  public void sayHello() {
+    if(isAlive) {
+      System.out.print(name+":  Hello, I'm " + name +"?\n");
+    }
+  }
+ 
+ 
+ 
+  /**Class Attributes**/
+  
+  //returtns the number of living persons
+  public static int numberLiving() {
+    return alive;   
+  }
+  
+  //returns the number of dead persons
+  public static int numberDead() {
+    int dead = population - alive;
+    return dead;
   }
   
   
-  public static String getFullName() {
-    return getRandomName()+" "+getRandomName();
-  }
-
-
-
-
-  public static void main(String[] args) {
-    
-    
-    //Makes a list for the people to be in 
-    ArrayList<Person> people = new ArrayList<Person>();
-      
-    //Decides the population of Random Ville  
-    Random rand = new Random();
-    int numberOfPeople = rand.nextInt(500) + 10;
-    
-    //Create person objects in the people array
-    for(int i = 0; i < numberOfPeople; i++) {
-      people.add(new Person(getFullName()));
-      //people.get(i).sayHello();
+  /**Class Actions**/
+  //causes every living person to say hello
+  public static void allSayHello() {
+    Person currentPerson = lastPerson;
+    while(currentPerson != null) {
+      currentPerson.sayHello();
+      currentPerson = currentPerson.previousPerson;
     }
-    
-    //Start of the story
-    System.out.print("In  small town called Random Ville there are "+Person.numberLiving()+" people living happy.\n");
-    
-    int murdererCount = rand.nextInt(10) + 1;
-    System.out.print("Sadly this town has a few("+murdererCount+") murderers in it who plan to go on a rampage!");
-    
-    //Picks the murderers
-    ArrayList<Integer> murderers = new ArrayList<Integer>();
-    for(int i = 0; i < murdererCount; i++) {
-      int temp = 0;
-      boolean alreadyMurderer = true;
-      while(alreadyMurderer) {
-			alreadyMurderer=false;
-			temp =rand.nextInt(numberOfPeople);
-			for(int j = 0; j < murderers.size(); j++) {
-  				if(temp == murderers.get(j)) {
-    				alreadyMurderer=true;
-				}
-			}
-      }
-      murderers.add(temp);
-	}
-    
-    
-    //System.out.println( Person.numberLiving() );
-    
-    while(murderers.size()>0){
-      for(int i = 0; i < murderers.size(); i++) {
-	boolean noMurder = true;
-	int temp = 0;
-	while(noMurder) {
-	  temp = rand.nextInt( people.size() );
-	  if(people.get(temp).isAlive()) {
-	    
-	    System.out.print( people.get( murderers.get( i ) ).name() + " murdered " + people.get( temp ).name()+"!\n");
-	    people.get( murderers.get( i ) ).murder( people.get( temp ) );
-	    //System.out.println(people.get(temp).name() );
-	    noMurder = false;
-	    
-	    for(int j = 0; j < murderers.size(); j++) {
-	      if(temp == murderers.get(j)) {
-		murderers.remove(j);
-		break;
-	      }
-	    }
-	    
-	  }
-	}
-      }
-      
-     
-    }
-    
-  //for(int j = 0; j < murderers.size(); j++) {
-     // murderers.get(j).name();
-    //}
-    //Person.allSay("hi");
-   // while(population>1)
-    Person.allSayHello();
-    System.out.println( Person.numberLiving() );
   }
 }
