14c7b83e496f60b68fbffe95b9d9d43b049b5c6b
SyonFox <kier.lightning@gmail.com>
2016-03-08 04:20:19 -0800

    Somthing

--- cpsc101/assignment_6/cpsc101/klindsay/lab6/savings/SavingsCaculator.java
+++ cpsc101/assignment_6/cpsc101/klindsay/lab6/savings/SavingsCaculator.java
@@ -1,5 +1,8 @@
-import javax.swing.*;
-import javax.swing.JPanel;
+import javax.swing.SpringLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.Container;
 
 public class SavingsCaculator extends JFrame {
   public SavingsCaculator() {
@@ -8,13 +11,29 @@ public class SavingsCaculator extends JFrame {
     setLocationRelativeTo(null);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    JPanel panel = new JPanel();
-    panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+    //JPanel panel = new JPanel();
+    //panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
 
-    panel.add(new JLabel("Enter Balance:"));
+    //panel.add(new JLabel("Enter Balance:"));
 
-    Container contentPane = getContentPane();
-    
+    Container contentPane = this.getContentPane();
+    SpringLayout layout = new SpringLayout();
+    contentPane.setLayout(layout);
+    JLabel balanceLB = new JLabel("Enter Balance:");
+    JTextField balanceTF = new JTextField("", 15);
+    contentPane.add(balanceLB);
+    contentPane.add(balanceTF);
+
+    layout.putConstraint(SpringLayout.WEST, balanceLB, 5,
+                         SpringLayout.WEST, contentPane);
+    layout.putConstraint(SpringLayout.NORTH, balanceLB, 5,
+                         SpringLayout.NORTH, contentPane);
+
+    layout.putConstraint(SpringLayout.WEST, balanceTF, 5,
+                         SpringLayout.EAST, balanceLB);
+    layout.putConstraint(SpringLayout.NORTH, balanceTF, 5,
+                         SpringLayout.NORTH, contentPane);
+    pack();
     setVisible(true);
   }
 
