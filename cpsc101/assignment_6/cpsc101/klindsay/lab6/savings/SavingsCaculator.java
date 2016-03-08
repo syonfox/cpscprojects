f30067d9cae2870c009b408793cb844077f8e0bb
SyonFox <kier.lightning@gmail.com>
2016-03-08 11:40:12 -0800

    :C

--- cpsc101/assignment_6/cpsc101/klindsay/lab6/savings/SavingsCaculator.java
+++ cpsc101/assignment_6/cpsc101/klindsay/lab6/savings/SavingsCaculator.java
@@ -1,13 +1,34 @@
-import javax.swing.SpringLayout;
+/**Somthing**/
+
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
-import java.awt.Container;
+
 
 public class SavingsCaculator extends JFrame {
+
+  JLabel balanceLB = new JLabel("Enter Balance:");
+  JTextField balanceTF = new JTextField("", 15);
+
+  JLabel yearsLB = new JLabel("Enter Years:");
+  JTextField yearsTF = new JTextField("", 15);
+
+  JLabel intrestLB = new JLabel("Enter Intrest(%):");
+  JTextField intrestTF = new JTextField("", 15);
+
+  JButton calculateBT = new JButton("Calculate");
+  JTextArea resultsTA = new JTextArea("Results", 20, 20);
+
   public SavingsCaculator() {
     setTitle("Am I Rich ... NO");
-    setSize(400, 600);
+    setSize(300, 600);
     setLocationRelativeTo(null);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -17,14 +38,33 @@ public class SavingsCaculator extends JFrame {
     //panel.add(new JLabel("Enter Balance:"));
 
     Container contentPane = this.getContentPane();
-    SpringLayout layout = new SpringLayout();
+    FlowLayout layout = new FlowLayout(FlowLayout.TRAILING, 10, 10);
     contentPane.setLayout(layout);
-    JLabel balanceLB = new JLabel("Enter Balance:");
-    JTextField balanceTF = new JTextField("", 15);
+
     contentPane.add(balanceLB);
     contentPane.add(balanceTF);
+    contentPane.add(yearsLB);
+    contentPane.add(yearsTF);
+    contentPane.add(intrestLB);
+    contentPane.add(intrestTF);
 
-    layout.putConstraint(SpringLayout.WEST, balanceLB, 5,
+
+
+    contentPane.add(calculateBT);
+
+    JTextArea resultsTA = new JTextArea("Results", 20, 20);
+    resultsTA.setEditable(false);
+
+    calculateBT.addActionListener( new ActionListener() {
+      @Override
+      public static void actionPreformed(ActionEvent e) {
+        calculate();
+      }
+    });
+
+    contentPane.add(resultsTA);
+
+  /*  layout.putConstraint(SpringLayout.WEST, balanceLB, 5,
                          SpringLayout.WEST, contentPane);
     layout.putConstraint(SpringLayout.NORTH, balanceLB, 5,
                          SpringLayout.NORTH, contentPane);
@@ -32,11 +72,30 @@ public class SavingsCaculator extends JFrame {
     layout.putConstraint(SpringLayout.WEST, balanceTF, 5,
                          SpringLayout.EAST, balanceLB);
     layout.putConstraint(SpringLayout.NORTH, balanceTF, 5,
-                         SpringLayout.NORTH, contentPane);
-    pack();
+                         SpringLayout.NORTH, contentPane);*/
+    //pack();
     setVisible(true);
   }
+  public void calculate() {
 
+    String balance = balanceTF.getText();
+    String years = yearsTF.getText();
+    String intrest = years.getText();
+
+    try {
+      Double bal = Double.parseDouble(balance);
+      Double yea = Double.parseDouble(years);
+      Double inr = Double.parseDouble(intrest);
+
+      resultsTA.setText("");
+      for(int i = 0; i < (int) yea; i++){
+        resultsTA.append(String.format("Year %4.2f : %.2f", i, bal * (1 + i*(inr / 100)) ));
+      }
+
+    } catch (NumberFormatException e) {
+      resultsTA.setText("You don ****** up dat input");
+    }
+  }
   public static void main(String[] args) {
     SavingsCaculator sc = new SavingsCaculator();
   }
